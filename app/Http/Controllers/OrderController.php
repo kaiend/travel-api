d9872b011d7e5ba99b0171b99ce99bbf672ed54f
aimy <1065215657@qq.com>
2017-11-22 10:45:01 +0800

    a

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -52,7 +52,8 @@ class OrderController extends  Controller
                     return ReturnMessage::success('订单类型未知' , '1006');
             }
             $bdata=json_decode(json_encode($data),true);
-            if(!empty($data)){
+            var_dump($bdata);
+            if( count($arr) != 0){
                 $final=ReturnMessage::toString($bdata);
 
                 return ReturnMessage::successData($final);
