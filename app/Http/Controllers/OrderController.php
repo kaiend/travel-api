3f62cea19a0c0205455e1360d0f1450be1f78f77
lxw <1065215657@qq.com>
2017-12-13 11:44:10 +0800

    q

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -142,7 +142,6 @@ class OrderController extends  Controller
                     return ReturnMessage::success('订单类型未知' , '1006');
             }
             $bdata=json_decode(json_encode($data),true);
-            dd($bdata);
             if( count($bdata) != 0){
                 $final=ReturnMessage::toString($bdata);
 
