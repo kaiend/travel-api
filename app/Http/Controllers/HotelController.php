ca36a7cbf0d4f89acc294db73391a64615236034
lxw <1065215657@qq.com>
2017-12-21 16:45:58 +0800

    a

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -518,7 +518,12 @@ class HotelController extends Controller
      */
     public function test()
     {
-        return $user_data= Hotel::getUserFirst(12);
+        $t = time();
+        $start = mktime(0,0,0,date("m",$t),date("d",$t),date("Y",$t));
+        $end = mktime(23,59,59,date("m",$t),date("d",$t),date("Y",$t));
+        print_r($start);
+        print_r($end);
+
     }
     /**
      * 出行地
