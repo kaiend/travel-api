fdfdd2c642315ea81bc1d918b92f6c98438e1d08
lxw <1065215657@qq.com>
2018-01-12 18:39:47 +0800

    a112vv

--- app/Http/Controllers/PushController.php
+++ app/Http/Controllers/PushController.php
@@ -85,7 +85,7 @@ class PushController extends Controller
         switch($arr['type']){
             //下单通知司机----下单通知酒店管理员
             case 'make':
-                if(!empty($chauffeur_data['jpush_code']) && $chauffeur_data['status_login'] ==1){
+                if(!empty($chauffeur_data['jpush_code'])){
 
                     $regids =$chauffeur_data['jpush_code'];
                     $message =[
