96ea4ed5140b638993900286ddd968af03915654
yyy <18311161659@163.com>
2018-02-06 18:26:20 +0800

    a

--- app/Http/Controllers/Wechat/OrderController.php
+++ app/Http/Controllers/Wechat/OrderController.php
@@ -33,6 +33,7 @@ class OrderController extends Controller
     public function createOrder( Request $request )
     {
         $input = WxorderValidator::topUp($request);
+        dd($input);die;
         if(empty($input['passenger_name'])){
             $input['passenger_name'] = $input['orders_name'];
         }
