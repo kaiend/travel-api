10b082ad9be1f63f80e89d823c041c8883546868
yyy <18311161659@163.com>
2018-02-23 15:52:01 +0800

    a

--- app/Http/Validators/UserValidator.php
+++ app/Http/Validators/UserValidator.php
@@ -235,7 +235,7 @@ class UserValidator
 		$only = ['id','travel_card','name','travel_card_number'];
 
 		$rules = [
-			'id' => 'required|exists:user,id',
+			'id' => 'required|exists:personal_user,id',
 			'name' => 'required',
 			'id_card' => 'required',
 		];
